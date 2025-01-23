06117bc6664ee1bec28be66374b2333e437104d6
kaizen <belebraz16@gmail.com>
2025-01-22 23:54:02 -0300

    feat: base entity

---
+++ src/main/java/com/bibliotex/catalog/domain/model/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.bibliotex.catalog.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
