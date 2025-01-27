187c56d6a8d138fbae2a261dc5dd40839dbf42c0
kaizen <belebraz16@gmail.com>
2025-01-27 19:01:21 -0300

    fix: annotations in author entity

--- src/main/java/com/bibliotex/catalog/domain/model/Author.java
+++ src/main/java/com/bibliotex/catalog/domain/model/Author.java
@@ -1,20 +1,27 @@
 package com.bibliotex.catalog.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Author extends BaseEntity {
     private String name;
     private String nationality;
 
-    @ManyToMany(mappedBy = "authors")
+    @JsonIgnore
+    @ManyToMany(mappedBy = "authors", fetch = FetchType.LAZY)
     private List<Catalog> catalogs = new ArrayList<>();
 }
