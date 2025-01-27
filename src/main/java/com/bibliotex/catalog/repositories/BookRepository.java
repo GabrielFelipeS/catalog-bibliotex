b9553e6f53d637e3fd30d2e53ba7f36f0eaafdf8
kaizen <belebraz16@gmail.com>
2025-01-27 19:13:51 -0300

    feat: findAll and existsBookByIsbn

--- src/main/java/com/bibliotex/catalog/repositories/BookRepository.java
+++ src/main/java/com/bibliotex/catalog/repositories/BookRepository.java
@@ -3,5 +3,10 @@ package com.bibliotex.catalog.repositories;
 import com.bibliotex.catalog.domain.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findAll();
+
+    boolean existsBookByIsbn(String isbn);
 }
