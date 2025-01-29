a9e97fd692399acdfa92f70877fb58228b40873c
kaizen <belebraz16@gmail.com>
2025-01-29 18:35:34 -0300

    feat: spring security

---
+++ src/main/java/com/bibliotex/catalog/infra/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.bibliotex.catalog.infra;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerLocation;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/catalog/books/", "/catalog/books/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/catalog/manga/", "/catalog/manga/{id}").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())));
+
+        return http.build();
+    }
+
+    public JwtDecoder jwtDecoder() {
+        System.err.println(issuerLocation);
+        return JwtDecoders.fromIssuerLocation(issuerLocation);
+    }
+}
+
