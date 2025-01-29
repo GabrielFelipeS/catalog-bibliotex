f9d5a7bb91312f25b13f7d942952f0e5fa6f6b75
kaizen <belebraz16@gmail.com>
2025-01-29 18:33:42 -0300

    feat: sendMessageCreate

--- src/main/java/com/bibliotex/catalog/services/KafkaService.java
+++ src/main/java/com/bibliotex/catalog/services/KafkaService.java
@@ -1,6 +1,7 @@
 package com.bibliotex.catalog.services;
 
 import com.bibliotex.catalog.domain.dto.response.BookResponse;
+import com.bibliotex.catalog.domain.dto.response.MangaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,10 @@ public class KafkaService {
 
     public void sendMessageCreate(BookResponse bookResponse) {
         this.sendMessage("catalog-create-book", bookResponse);
+    }
+
+    public void sendMessageCreate(MangaResponse mangaResponse) {
+        this.sendMessage("catalog-create-manga", mangaResponse);
     }
 
 }
