b27b6fff81e9e348bb88e92b53c45005ed540e90
kaizen <belebraz16@gmail.com>
2025-01-29 21:29:16 -0300

    feat: ComicResponse

---
+++ src/main/java/com/bibliotex/catalog/domain/dto/response/ComicResponse.java
@@ -0,0 +1,21 @@
+package com.bibliotex.catalog.domain.dto.response;
+
+import com.bibliotex.catalog.domain.model.Author;
+import com.bibliotex.catalog.domain.model.Publisher;
+
+import java.util.List;
+
+public record ComicResponse(
+        Long id,
+        String title,
+        String description,
+        Integer pages,
+        String language,
+        Integer edition,
+        Integer yearOfRelease,
+        String imageUrl,
+        List<Author> authors,
+        Publisher publisher,
+        String universe
+) {
+}
