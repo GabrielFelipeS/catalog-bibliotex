11ee4cd34ff89a93175ccdc9dcd1a376225605d7
kaizen <belebraz16@gmail.com>
2025-01-29 21:30:20 -0300

    fix: remove authors and publisher

--- src/main/java/com/bibliotex/catalog/domain/model/Manga.java
+++ src/main/java/com/bibliotex/catalog/domain/model/Manga.java
@@ -1,11 +1,10 @@
 package com.bibliotex.catalog.domain.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,25 +12,13 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Manga extends Catalog{
-    private Boolean isOngoing;
+    private Boolean isOnGoing;
     private String magazine;
 
-    @ManyToMany
-    @JoinTable(
-            name = "author_book",
-            joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "author_id")
-    )
-    private List<Author> authors = new ArrayList<>();
-
-    @ManyToOne
-    @JoinColumn(name = "publisher_id")
-    private Publisher publisher;
-
     public Manga(String title, String description, Integer pages, String language,
-                Integer edtiion, Integer yearOfRelease, String imageUrl, List<Author> authors, Publisher publisher, Boolean isOngoing, String magazine) {
+                 Integer edtiion, Integer yearOfRelease, String imageUrl, List<Author> authors, Publisher publisher, Boolean isOnGoing, String magazine) {
         super(title, description, pages, language, edtiion, yearOfRelease, imageUrl, authors, publisher);
-        this.isOngoing = isOngoing;
+        this.isOnGoing = isOnGoing;
         this.magazine = magazine;
     }
 }
