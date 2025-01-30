10ad8d6ad4e7be9988e8f6468c00c2917970661c
kaizen <belebraz16@gmail.com>
2025-01-29 21:28:55 -0300

    feat: findAll in ComicRepository

--- src/main/java/com/bibliotex/catalog/repositories/ComicRepository.java
+++ src/main/java/com/bibliotex/catalog/repositories/ComicRepository.java
@@ -3,5 +3,8 @@ package com.bibliotex.catalog.repositories;
 import com.bibliotex.catalog.domain.model.Comic;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ComicRepository extends CrudRepository<Comic, Long> {
+    List<Comic> findAll();
 }
