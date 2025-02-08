a2da22fb21de9873080502568e85d27dbd86f862
kaizen <belebraz16@gmail.com>
2025-02-08 19:06:27 -0300

    refactor: add ValidationMessages

--- src/main/java/com/bibliotex/catalog/domain/dto/request/BookRequest.java
+++ src/main/java/com/bibliotex/catalog/domain/dto/request/BookRequest.java
@@ -1,42 +1,44 @@
 package com.bibliotex.catalog.domain.dto.request;
 
+import com.bibliotex.catalog.domain.messsages.ValidationMessages;
 import jakarta.validation.constraints.*;
 
 import java.util.List;
 
 
 public record BookRequest (
-        @NotBlank(message = "O titulo não deveria ser nulo ou vazio")
+        @NotBlank(message = ValidationMessages.TITLE_NOT_BLANK)
         String title,
 
-        @NotBlank(message = "A descrição não deveria ser nula ou vazia")
+        @NotBlank(message = ValidationMessages.DESCRIPTION_NOT_BLANK)
         String description,
 
-        @NotNull(message = "A páginas não deve ser nulo")
-        @Positive(message = "A quantidade de páginas deve ser maior que zero")
+        @NotNull(message = ValidationMessages.PAGES_NOT_NULL)
+        @Positive(message = ValidationMessages.PAGES_QUANTITY_MUST_ZERO)
         Integer pages,
 
-        @NotBlank(message = "A lingua não deveria ser nula ou vazia")
+        @NotBlank(message = ValidationMessages.LANGUAGE_NOT_BLANK)
         String language,
 
-        @NotNull(message = "A edição não deve ser nulo")
-        @Positive(message = "A edição deve ser maior que zero")
+        @NotNull(message = ValidationMessages.EDITION_NOT_NULL)
+        @Positive(message = ValidationMessages.EDITION_QUANTITY_MUST_ZERO)
         Integer edition,
 
-        @NotNull(message = "O ano de lançamento não deve ser nulo")
-        @Positive(message = "A quantidade de páginas deve ser maior que zero")
+        @NotNull(message = ValidationMessages.YEAR_OF_RELEASE_NOT_NULL)
+        @Positive(message = ValidationMessages.YEAR_OF_RELEASE_MUST_ZERO)
         Integer yearOfRelease,
 
-        @NotEmpty(message = "A lista de IDs de autores não deve ser vazia")
-        @NotNull(message = "A lista de IDs dos autores não deve ser nulo")
-        List<@Positive(message = "O id do autor deve ser maior que zero") Long> authorsIds,
+        @NotEmpty(message = ValidationMessages.AUTHORS_ID_NOT_EMPTY)
+        @NotNull(message = ValidationMessages.AUTHORS_IDS_NOT_NULL)
+        List<@Positive(message = ValidationMessages.AUTHOR_ID_MUST_ZERO) Long> authorsIds,
 
-        @NotNull(message = "A editora não deve ser nulo")
-        @Positive(message = "A quantidade de páginas deve ser maior que zero")
+        @NotNull(message = ValidationMessages.PUBLISHER_ID_NOT_NULL)
+        @Positive(message = ValidationMessages.PUBLISHER_ID_MUST_ZERO)
         Long publisherId,
 
-        @NotBlank(message = "A imageUrl não deveria ser nulo ou vazio")
-        @Pattern(regexp = "^http(|s)://books\\.google\\.com/books/content\\?id=[a-zA-Z0-9_-]+(&.*)?$", message = "O endereço da imagem não é valido, por favor insira outro")
+        @NotBlank(message = ValidationMessages.IMAGE_NOT_BLANK)
+        @Pattern(message = ValidationMessages.IMAGE_VALID,
+                regexp = "^http(|s)://books\\.google\\.com/books/content\\?id=[a-zA-Z0-9_-]+(&.*)?$")
         String imageUrl,
 
         @NotBlank(message = "O ISBN não deveria ser nulo ou vazio")
